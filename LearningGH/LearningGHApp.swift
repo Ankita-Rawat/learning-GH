1a8a2229ae063e748c567297c591425656b71dec
Ankita-Rawat <go2ankitarawat@gmail.com>
2021-11-14 08:54:30 +0530

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Bharat Singh on 14/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
